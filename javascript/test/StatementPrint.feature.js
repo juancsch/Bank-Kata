3a9085ed8a1264e1852a77ce59a8bfa90049e705
Juan Carlos Serrano <juancsch@gmail.com>
2018-07-06 15:46:04 +0200

    Acceptance test for feature falling for correct reason

--- javascript/test/StatementPrint.feature.js
+++ javascript/test/StatementPrint.feature.js
@@ -1,7 +1,25 @@
-describe('Feature ...', () => {
+const Account = require('../lib/Account')
 
-	test('should ...', () => {
+describe('Statement print feature', () => {
 
-		expect(true).toBe(true)
+	test('should print statement containing all transactions', () => {
+
+		const view = {
+			printLine: jest.fn()
+		}
+
+		const account = Account()
+
+		account.deposit(1000)
+		account.withdrawal(100)
+		account.deposit(500)
+
+		account.printStatement()
+
+		expect(view.printLine.mock.calls.length).toBe(4)
+		expect(view.printLine.mock.calls[0][0]).toBe('DATE | AMOUNT | BALANCE')
+		expect(view.printLine.mock.calls[1][0]).toBe('10/04/2014 | 500.00 | 1400.00')
+		expect(view.printLine.mock.calls[2][0]).toBe('02/04/2014 | -100.00 | 900.00')
+		expect(view.printLine.mock.calls[3][0]).toBe('01/04/2014 | 1000.00 | 1000.00')
 	})
 })
