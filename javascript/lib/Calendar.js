f00bb117fb3356a4bec7a6cca4a88360413fd422
Juan Carlos Serrano <juancsch@gmail.com>
2018-07-08 13:07:57 +0200

    Calendat specification: print ok

--- javascript/lib/Calendar.js
+++ javascript/lib/Calendar.js
@@ -1,3 +1,20 @@
+function formatted (date = new Date()) {
+
+	let month = '' + (date.getMonth() + 1)
+	let day = '' + date.getDate()
+	const year = date.getFullYear()
+
+	if (month.length < 2) month = '0' + month
+	if (day.length < 2) day = '0' + day
+
+	return [day, month, year].join('/')
+}
+
+function dateAsString () {
+	const today = new Date()
+	return formatted(today)
+}
+
 module.exports = {
-	dateAsString: () => {}
+	dateAsString
 }
--- javascript/test/Calendar.test.js
+++ javascript/test/Calendar.test.js
@@ -4,6 +4,6 @@ describe('Calendar behaviour', () => {
 
 	test('should return formatter date for today', () => {
 
-		expect(calendar.dateAsString()).toEqual('09/07/2018')
+		expect(calendar.dateAsString()).toEqual('08/07/2018')
 	})
 })
