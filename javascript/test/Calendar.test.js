3be49eee9e9fc880af7bfd7557bf9407c5783350
Juan Carlos Serrano <juancsch@gmail.com>
2018-07-08 13:04:20 +0200

    Calendat specification: print falling

---
+++ javascript/test/Calendar.test.js
@@ -0,0 +1,9 @@
+const calendar = require('../lib/Calendar')
+
+describe('Calendar behaviour', () => {
+
+	test('should return formatter date for today', () => {
+
+		expect(calendar.dateAsString()).toEqual('09/07/2018')
+	})
+})
